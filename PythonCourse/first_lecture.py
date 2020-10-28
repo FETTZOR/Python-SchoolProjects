033464b5773aee01aea1d5bdb38c9df2ec4ffc83
Emil Kh <45365067+FETTZOR@users.noreply.github.com>
2020-10-28 12:07:23 +0200

    first_lecture_easy2_upd

--- PythonCourse/first_lecture.py
+++ PythonCourse/first_lecture.py
@@ -1,10 +1,14 @@
 #! /usr/bin/python3
 
-def function_with_return(input_to_function1, input_to_function2):
-    variable_that_we_return_from_function = input_to_function1 + input_to_function2
+# easy1
+def function_with_return(input_to_function1, input_to_function2, input_to_function3):
+    variable_that_we_return_from_function = input_to_function1 + input_to_function2 + input_to_function3
     return variable_that_we_return_from_function
 
 
 # call function
-answer_to_print1 = function_with_return(1, 4)
+answer_to_print1 = function_with_return(1, 4, 5)
 print(answer_to_print1)
+# easy2:
+answer_to_print2 = function_with_return(1, 2, 3)
+print(answer_to_print2)
