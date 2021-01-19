cc330e6baa10c64e0aa707208444319fd4c1441a
Emil Kh <45365067+FETTZOR@users.noreply.github.com>
2021-01-19 12:07:40 +0200

    oop exercise 1 added

---
+++ Object-Oriented-Programming/dotnet_new_project/firstLecture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace firstLecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string name = "Emil";
+
+            Console.WriteLine(name);
+
+            int age = 34;
+            double someNumber;
+            someNumber = 3.23123;
+
+            float floatVar = 3.23123f;
+
+            bool asd = true;
+
+            Console.WriteLine(asd);
+
+            Console.WriteLine($"asdad {1 + 1}, {age}");
+
+            SayHello();
+
+            double multiplied = Multiply(2.0);
+            Console.WriteLine(multiplied);
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static double Multiply(double n)
+        {
+            return n * 2;
+        }
+    }
+}
---
+++ Object-Oriented-Programming/exercises_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercises_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Emil";
+
+            int myAge = 24;
+
+            double PI = 3.14;
+
+            Console.WriteLine($"{myName} \n{myAge} \n{PI} \n");
+
+            UsefulFunction();
+
+            int multiplier = MultiplyByTwo(3);
+            Console.WriteLine(multiplier);
+        }
+
+        static void UsefulFunction()
+        {
+            Console.WriteLine("Function executed!");
+        }
+
+        static int MultiplyByTwo(int n)
+        {
+            return n * 2;
+        }
+    }
+}
